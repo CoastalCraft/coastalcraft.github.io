8d39af8175fe4de220af655ebea20e8d2a624813
BSavage81 <beau.j.savage@gmail.com>
2020-12-04 18:39:43 -0600

    initial

---
+++ poi/customsData.js
@@ -0,0 +1,41 @@
+// NOTE: Please only modify player attributes below such as name, color, and visible
+// To hide a player's marker, change their 'visible' variable from 'true' to 'false'
+// This file is automatically updated, and if other parts are changed, it will fail to update
+// Changes will only be read from the /map/playersData.js file - /update/playersData.js will be overwritten, so don't make changes there
+var customsData = {
+  "customs": [
+    {
+      "name": "Spawn Ship",
+      "dimensionId": 0,
+      "position": [
+        -61,
+        64,
+        80
+      ],
+      "color": "Black",
+      "visible": true
+    },
+    {
+      "name": "Event Ship",
+      "dimensionId": 0,
+      "position": [
+        33,
+        64,
+        101
+      ],
+      "color": "Black",
+      "visible": true
+    },
+    {
+      "name": "Tim, Tom, and Tam's Ship",
+      "dimensionId": 0,
+      "position": [
+        98,
+        64,
+        95
+      ],
+      "color": "Black",
+      "visible": true
+    },
+  ]
+};
