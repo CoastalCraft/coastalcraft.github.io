33b50edbf76043c298ed33dbb8224e4605989429
BSavage81 <beau.j.savage@gmail.com>
2022-02-17 11:04:27 -0600

    map update 021622

--- custom.markers.js
+++ custom.markers.js
@@ -1,12 +1,17 @@
 /*
+
 This is a JavaScript file you can edit to add custom markers to the map.
 uNmINeD does not overwrite this file during map generation.
+
 Steps:
+
     1. Edit this file using Notepad or a code editor (do not use document editors like Microsoft Word)
     2. Change the line "isEnabled: false," to "isEnabled: true," to display the markers
     3. Change or remove the example markers
     4. Add your own markers
+
 Marker format:
+
     {
         x: X coordinate of the marker (in Minecraft block units),
         z: Z coordinate of the marker (in Minecraft block units),
@@ -19,12 +24,16 @@ Marker format:
         offsetY: vertical pixel offset of the text,
         font: text font in HTML/CSS format (in quotes),
     },
+
 Things to keep in mind:
+
 * There are opening and closing brackets for each marker "{" and "}"
 * Property names are case sensitive (i.e. "textColor" is okay, "TextColor" is not)
 * There is a comma (",") at the end of each line except the opening brackets ("{")
+
 You can use https://mapmarker.io/editor to generate custom pin images.
 Use the imageScale property if the pin image is too large.
+
 */
 
 UnminedCustomMarkers = {
@@ -33,90 +42,45 @@ UnminedCustomMarkers = {
 
     markers: [
 
+        // Example 1: Simple marker
+        {
+            x: -200,
+            z: -200,
+            image: "custom.pin.png",
+            imageAnchor: [0.5, 1],
+            imageScale: 0.5,
+        },
+
+        // Example 2: Marker with text
         {
             x: 0,
             z: 0,
-            minmapzoom: 2,
-            maxmapzoom: 5,
-            text: "Spawn Portal\n\uf557",
-            textColor: "#a839a3",
-            strokeColor: "#ffffff",
+            image: "custom.pin.png",
+            imageAnchor: [0.5, 1],
+            imageScale: 0.5,
+            text: "Marker with text",
+            textColor: "red", 
             offsetX: 0,
-            offsetY: 0,
-            font: "italic 900 20px 'Font Awesome 5 Free'",
+            offsetY: 20,
+            font: "bold 20px Calibri,sans serif",
         },
+
+        // Example 3: Text only
         {
-            x: 560,
-            z: -240,
-            minmapzoom: 2,
-            maxmapzoom: 5,
-            text: "North Spawn Portal\n\uf557",
-            textColor: "#a839a3",
-            strokeColor: "#ffffff",
+            x: 200,
+            z: 200,
+            text: "Text only", 
+            textColor: "yellow", 
             offsetX: 0,
-            offsetY: 0,
-            font: "italic 900 20px 'Font Awesome 5 Free'",
+            offsetY: 0, 
+            font: "bold 50px Calibri,sans serif",
         },
-        {
-            x: 480,
-            z: 400,
-            minmapzoom: 2,
-            maxmapzoom: 5,
-            text: "East Spawn Portal\n\uf557",
-            textColor: "#a839a3",
-            strokeColor: "#ffffff",
-            offsetX: 0,
-            offsetY: 0,
-            font: "italic 900 20px 'Font Awesome 5 Free'",
-        },
-        {
-            x: 130,
-            z: -15,
-            minmapzoom: 3,
-            maxmapzoom: 5,
-            text: "Tree Farm\n\uf1bb",
-            textColor: "#0be016",
-            strokeColor: "#ffffff",
-            offsetX: 0,
-            offsetY: 0,
-            font: "italic 900 20px 'Font Awesome 5 Free'",
-        },
-        {
-            x: 115,
-            z: 78,
-            minmapzoom: 2,
-            maxmapzoom: 5,
-            text: "\uf13d\nSpawn Ship",
-            textColor: "#14959e",
-            strokeColor: "#ffffff",
-            offsetX: 0,
-            offsetY: 0,
-            font: "italic 900 20px 'Font Awesome 5 Free'",
-        },
-        {
-            x: 70,
-            z: 32,
-            minmapzoom: 3,
-            maxmapzoom: 5,
-            text: "\uf13d\nTim, Tom, Tam\n\uf51e,\uf522",
-            textColor: "#14959e",
-            strokeColor: "#ffffff",
-            offsetX: 0,
-            offsetY: 0,
-            font: "italic 900 12px 'Font Awesome 5 Free'",
-        },
-        {
-            x: 6,
-            z: 67,
-            minmapzoom: 2,
-            maxmapzoom: 5,
-            text: "\uf13d\nEvent Ship",
-            textColor: "#14959e",
-            strokeColor: "#ffffff",
-            offsetX: 0,
-            offsetY: 0,
-            font: "italic 900 12px 'Font Awesome 5 Free'",
-        }
-        
+
+        // add your markers here
+
+
+
+
+        // do not delete the following two closing brackets
     ]
 }
